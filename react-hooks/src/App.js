a6cc1095e89fdda41b408b79c081503b6956f2fb
gka0903 <ham3252@naver.com>
2023-01-27 11:02:09 +0900

    useInput

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,16 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import React ,{ useState } from 'react'
+import React  from 'react'
+// import { useInput } from './useInput/useInput'
 
 function App() {
-  const [item, setItem] = useState(1);
-  return (
-    <div className="App">
-      <h1>{item}</h1>
-      <button onClick={() => setItem(item + 1)}>+</button>
-      <button onClick={() => setItem(item - 1)}>-</button>
-    </div>
-  );
+
+    return (
+        <>
+            <h1>Hello</h1>
+
+        </>
+    );
 }
 
 export default App;
