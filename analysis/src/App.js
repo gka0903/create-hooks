5059e90cb1df8834ac97ad3b5bc28dcdcc2de8dd
gka0903 <ham3252@naver.com>
2023-01-27 11:03:37 +0900

    changes

--- analysis/src/App.js
+++ analysis/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+
 import './App.css';
+import styled from 'styled-components';
+import { Bar } from 'react-chartjs-2';
+import { useState, useEffect } from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+} from "chart.js";
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
 
 function App() {
-  return (
+    const [chartData, setChartData] = useState({
+        datasets : [],
+    });
+    const [chartOptions, setChartOptions] = useState({});
+
+    useEffect(() => {
+        setChartData({
+            labels : ["1", "2", "3", "4"],
+            datasets: [
+                {
+                    label : "Member",
+                    data : [60, 70, 100, 300],
+                    borderColor : "rgb(53, 162, 235)",
+                    backgroundColor : "rgb(53, 162, 235, 0.4)"
+                },
+            ],
+        });
+        setChartOptions({
+            responsive : true,
+            plugins : {
+                legend : {
+                    position : "top"
+                },
+                title : {
+                    display : true,
+                    text : "Country"
+                }
+            }
+        })
+    })
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+          <Bar options={chartOptions} data={chartData}/>
+      </Container>
     </div>
   );
 }
+
+const Container = styled.div`
+  width: 90vw;
+  max-width: 900px;
+`;
 
 export default App;
